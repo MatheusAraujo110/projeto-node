0f1e017334086a2fec71ae13b91234be1c73f169
MatheusAraujo110 <theusnascimento386@gmail.com>
2024-06-25 13:46:04 -0300

    query params, route params e body params

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 import express from 'express';
 
 const app = express()
+app.use(express.json())
 
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios/:id', (req, res) => {
+    console.log(req)
+
     res.send("Rota acessada com sucesso!")
 })
 
 app.post('/usuarios', (req, res) => {
+    console.log(req)
+
     res.send("Olá!")
 })
 
